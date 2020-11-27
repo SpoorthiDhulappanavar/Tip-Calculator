e66d4f509b599ce0c975c753f3bd49d695ed14b5
Spoorthi Dhulappanavar <sxd5682@psu.edu>
2020-11-27 15:37:58 +0530

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Spoorthi Dhulappanavar on 27/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
